7d167f6652cc85fb5cc6918296bbfae9e0e9a996
baihang <baihang@shiqichuban.com>
2020-08-21 17:48:16 +0800

    完善数据解析部分

---
+++ app/src/main/java/com/example/healthy/data/HeartOneData.kt
@@ -0,0 +1,13 @@
+package com.example.healthy.data
+
+/**
+ * 单导联心电数据
+ */
+class HeartOneData : BaseData() {
+
+    init {
+        headData = arrayOf(0xAA, 0xAA, 0x07, 0x0B)
+        dataInit()
+    }
+
+}
