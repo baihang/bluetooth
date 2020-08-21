55c6aaa9d16c197e72d937ec8e894dd2997db395
baihang <baihang@shiqichuban.com>
2020-08-21 17:48:16 +0800

    完善数据解析部分

---
+++ app/src/main/java/com/example/healthy/data/BaseData.kt
@@ -0,0 +1,41 @@
+package com.example.healthy.data
+
+import android.util.Log
+
+abstract class BaseData {
+
+    companion object {
+        private const val TAG = "BaseData"
+        const val HEAD: Short = 0xAA
+        const val HEAD_LENGTH = 4
+
+        fun getDataType(value1: Short, value2: Short): BaseData? {
+            val type: Int = value1.toInt().shr(8) + value2
+            Log.e(TAG, "type = $type")
+            return when (type) {
+                0xAA07 -> {
+                    HeartOneData()
+                }
+                0xAA06 -> {
+                    PulseData()
+                }
+                0xAB07 -> {
+                    HeartThreeData()
+                }
+                else -> {
+                    null
+                }
+            }
+        }
+    }
+
+    lateinit var headData: Array<Short>
+    lateinit var bodyData: Array<Short>
+    lateinit var trialData: Array<Short>
+
+    fun dataInit() {
+        bodyData = Array(headData[3].toInt(), init = { 0 })
+        trialData = arrayOf(0)
+    }
+
+}
